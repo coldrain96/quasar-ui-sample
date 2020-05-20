5d99915e4fb93deacd2a32286ea5075faad66e77
coldrain96 <Justtryagain961@gmail.com>
2020-05-20 18:10:01 +0500

    created document page

---
+++ src/pages/Documents.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+    <div class="documents">
+      <div class="q-pa-md">
+        <q-table
+          title="Treats"
+          :data="getAllItems"
+          :columns="getColumns"
+          row-key="name"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    computed: mapGetters([
+      'getColumns',
+      'getAllItems'
+    ])
+  }
+</script>

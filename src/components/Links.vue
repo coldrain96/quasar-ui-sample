1ada976cd420f681fa44c210f1b3e3b6144fa787
coldrain96 <Justtryagain961@gmail.com>
2020-05-20 18:09:25 +0500

    created Links component

---
+++ src/components/Links.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-tabs>
+    <q-route-tab
+      :to="link"
+      exact
+    >
+      <q-item
+        clickable
+        tag="a"
+      >
+        <q-item-section
+          v-if="img"
+          avatar
+        >
+          <q-icon :name="img"/>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-route-tab>
+  </q-tabs>
+</template>
+
+<script>
+  export default {
+    name: 'Links',
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+
+      link: {
+        type: String,
+        default: '#'
+      },
+
+      img: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
